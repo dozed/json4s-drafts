d91e2cb86050848d9fd8c508867b509e43aaadd7
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-03-13 21:47:44 +0100

    Better validation failure example.

--- src/test/scala/ParseExample3.scala
+++ src/test/scala/ParseExample3.scala
@@ -13,22 +13,36 @@ object ParseExample3 extends App {
       """
         |{
         |  "i": 100,
-        |  "s": "hey ho"
+        |  "s": "hey ho",
+        |  "x": ["foo", "bar"],
+        |  "a": {
+        |    "x": 200
+        |  }
         |}
       """.stripMargin
 
-  case class Foo(i: Int, s: String)
+  case class A(x: Int)
+  case class Foo(i: Int, s: String, x: List[String], a: A)
 
-  implicit val aInstance = JSONRExt[Foo]
-
-  val foo = Foo(0, "foo")
+  val foo = Foo(0, "foo", List("foo", "bar"), A(200))
 
   val res: Result[Foo] = parseJson(json).validate[Foo]
-  // Success(Foo(100,hey ho))
+  // Success(Foo(100,hey ho,List(foo, bar),A(200)))
   println(res)
 
-  val res2: Result[Foo] = parseJson("{}").validate[Foo]
-  // Failure(NonEmptyList(UnexpectedJSONError(JNothing,class org.json4s.JsonAST$JInt), UnexpectedJSONError(JNothing,class org.json4s.JsonAST$JString)))
+  val json2 =
+    parseJson("""
+      |{
+      |  "i": 100,
+      |  "s": "hey ho",
+      |  "x": "foo",
+      |  "a": 200
+      |}
+    """.stripMargin)
+
+
+  val res2: Result[Foo] = json2.validate[Foo]
+  // Failure(NonEmptyList(UnexpectedJSONError(JString(foo),class org.json4s.JsonAST$JArray), UncategorizedError(x,Could not read value,List())))
   println(res2)
 
 }
