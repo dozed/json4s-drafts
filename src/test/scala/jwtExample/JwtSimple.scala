a1958a475f04954dc7d9289410522708a0981b24
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-03-25 15:04:53 +0100

    Factored out JWS.

--- src/test/scala/jwtExample/JwtSimple.scala
+++ src/test/scala/jwtExample/JwtSimple.scala
@@ -1,25 +1,53 @@
 package jwtExample
 
-import scalaz._, Scalaz._
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import java.util.Base64
 
+import scalaz._
+import Scalaz._
 import org.json4s._
-import org.json4s.jackson.{prettyJson, compactJson, parseJson, parseJsonOpt}
+import org.json4s.jackson.{compactJson, parseJson, parseJsonOpt, prettyJson}
 import org.json4s.ext.scalaz.JsonScalaz._
-
 import shapeless._
 import shapeless.newtype._
 import shapeless.{:+:, Coproduct, _}
+import shapeless.syntax.std.tuple._
+import shapeless.syntax.std.traversable._
+
+import scala.util.Try
 
 object JwtSimple extends App {
 
   // types
 
-  case class Jwt(headers: List[Header], claims: List[Claim])
+  case class Jws[A:JSONW](
+    header: List[Header],
+    payload: A,
+    signature: JwsSignature
+  ) {
 
-  // type StringOrList = Coproduct.`String, List[String]`.T
-  type StringOrList = String :+: List[String] :+: CNil
+    lazy val alg: Algorithm = header.collectFirst { case Header.Alg(x) => x}.get
 
-  def stringOrList = Coproduct[StringOrList]
+  }
+
+  type JwsCompact = String
+  type JwsSignature = String
+  type Jwt = List[Claim]
+  type JoseHeader = List[Header]
+
+  sealed trait Header
+
+  object Header {
+
+    case class Typ(value: String) extends Header
+    case class Cty(value: String) extends Header
+    case class Alg(value: Algorithm) extends Header
+
+  }
+
+
+
 
   sealed trait Algorithm
 
@@ -46,18 +74,28 @@ object JwtSimple extends App {
     case class Jti(value: String) extends Claim
 
     // user-defined claims
+    // TODO value
     case class Custom(name: String, value: String) extends Claim
 
   }
 
 
-  sealed trait Header
-  object Header {
 
-    case class Typ(value: String) extends Header
-    case class Cty(value: String) extends Header
-    case class Alg(value: Algorithm) extends Header
 
+
+  // type StringOrList = Coproduct.`String, List[String]`.T
+  type StringOrList = String :+: List[String] :+: CNil
+
+  def stringOrList = Coproduct[StringOrList]
+
+
+
+  sealed trait JwsError
+
+  object JwsError {
+    case object NoAlgHeader extends JwsError
+    case object InvalidJwsCompact extends JwsError
+    case object InvalidSignature extends JwsError
   }
 
 
@@ -115,13 +153,12 @@ object JwtSimple extends App {
     case Algorithm.NONE => "NONE"
   }
 
-  implicit val algorithmShows = Show.show[Algorithm] {
+  implicit val algorithmShow = Show.show[Algorithm] {
     case Algorithm.HS256 => "HmacSHA256"
     case Algorithm.HS384 => "HmacSHA384"
     case Algorithm.HS512 => "HmacSHA512"
     case Algorithm.NONE => "none"
   }
-
 
   val readHeader: (String, JValue) => Result[Header] = {
     case ("typ", v) => v.validate[String].map(Header.Typ)
@@ -136,7 +173,6 @@ object JwtSimple extends App {
     case Header.Alg(x) => ("alg", x.toJson)
   }
 
-
   implicit lazy val headersRead = read[List[Header]] {
     case x: JObject => x.obj.map(x => readHeader.tupled(x)).sequence[Result, Header]
     case json => Fail.unexpected(json, classOf[JObject])
@@ -147,10 +183,94 @@ object JwtSimple extends App {
   }
 
 
+  object Jws {
+
+    def validate[A](compact: JwsCompact): JwsError \/ Unit = {
+      for {
+        jws1 <- Jws.decode[List[Claim]](compact) \/> JwsError.InvalidJwsCompact
+        jws2 = Jwt.sign(jws1.payload, secret, jws1.alg)
+        _ <- {
+          if (jws1.signature === jws2.signature) ().right
+          else JwsError.InvalidSignature.left
+        }
+      } yield ()
+    }
+
+    def decode[A:JSONR:JSONW](jws: JwsCompact): Option[Jws[A]] = {
+      val xs = jws.split('.').toList
+      for {
+        (headerText, payloadText, signature) <- xs.toHList[String :: String :: String :: HNil].map(_.tupled)
+        header <- decodeFromBase64[List[Header]](headerText)
+        claim <- decodeFromBase64[A](payloadText)
+        // alg <- headers.collectFirst { case Header.Alg(x) => x }  // JWS only valid with an alg header ?
+      } yield {
+        Jws(header, claim, signature)
+      }
+    }
+
+    def encodeHeaderAndPayload[A:JSONW](header: List[Header], payload: A): String = {
+      val encodedHeader = encodeBase64Url(compactJson(header.toJson))
+      val encodedPayload = encodeBase64Url(compactJson(payload.toJson))
+      val encodedHeaderAndPayload = s"$encodedHeader.$encodedPayload"
+
+      encodedHeaderAndPayload
+    }
+
+    def computeMac(encodedHeaderAndPayload: String, algorithm: Algorithm, secret: String): JwsSignature = {
+      def hmac(alg: Algorithm) = {
+        val mac: Mac = Mac.getInstance(alg.shows)
+        mac.init(new SecretKeySpec(secret.getBytes("utf-8"), alg.shows))
+        encodeBase64Url(mac.doFinal(encodedHeaderAndPayload.getBytes("utf-8")))
+      }
+
+      algorithm match {
+        case Algorithm.HS256 => hmac(Algorithm.HS256)
+        case Algorithm.HS384 => hmac(Algorithm.HS384)
+        case Algorithm.HS512 => hmac(Algorithm.HS512)
+        case Algorithm.NONE => ""
+      }
+    }
+
+    def decodeFromBase64[A:JSONR](base64: String): Option[A] = {
+      for {
+        text <- Try(Base64.getDecoder.decode(base64)).toOption
+        json <- parseJsonOpt(new String(text))
+        a <- json.validate[A].toOption
+      } yield a
+    }
+
+    def decodeBase64(subject: String): String = new String(Base64.getDecoder.decode(subject))
+
+    def encodeBase64Url(subject: String): String = Base64.getEncoder.encodeToString(subject.getBytes("utf-8"))
+
+    def encodeBase64Url(subject: Array[Byte]): String = Base64.getEncoder.encodeToString(subject)
+
+  }
+
+  object Jwt {
+
+    def sign(claims: List[Claim], secret: String, alg: Algorithm): Jws[Jwt] = {
+      val headers = List[Header](
+        Header.Typ("JWT"),
+        Header.Alg(alg)
+      )
+
+      val headerAndPayload = Jws.encodeHeaderAndPayload(headers, claims)
+      val mac = Jws.computeMac(headerAndPayload, alg, secret)
+      Jws[Jwt](headers, claims, mac)
+    }
+
+    def compact(claims: List[Claim], secret: String, alg: Algorithm): JwsCompact = {
+      val jws = sign(claims, secret, alg)
+      val headerAndPayload = Jws.encodeHeaderAndPayload(jws.header, jws.payload)
+      s"$headerAndPayload.${jws.signature}"
+    }
+
+  }
 
   // example
 
-  val token = Jwt(
+  val jws1 = Jws[Jwt](
     List(
       Header.Typ("foo"),
       Header.Cty("bar"),
@@ -166,23 +286,41 @@ object JwtSimple extends App {
       Claim.Iat(42),
       Claim.Jti("jti"),
       Claim.Custom("foo", "\"bar\"")
-    )
+    ),
+    "foo"
   )
 
 
 
-  val json = token.toJson
-  val token2 = json.read[Jwt] getOrElse ???
+  val json = jws1.toJson
+  val sig2 = json.read[Jws[Jwt]] getOrElse ???
 
   println(prettyJson(json))
   // TODO order of headers/claims
-  println(token == token2)
-  println(token)
-  println(token2)
+  println(jws1 == sig2)
+  println(jws1)
+  println(sig2)
+
+  val secret = "thequickbrownfoxjumpsoverthelazydog"
+
+  println(Jwt.sign(jws1.payload, secret, Algorithm.HS512))
+
+  val s = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJpc3MiLCJzdWIiOiJzdWIiLCJhdWQiOiJhdWQiLCJhdWQiOlsiYXVkIiwiYXVkMiJdLCJleHAiOjQyLCJuYmYiOjQyLCJpYXQiOjQyLCJqdGkiOiJqdGkiLCJmb28iOiJiYXIifQ==.9SCDyruJ9p0SGkzGdMdBc6O5wLK1G7MKtGuNABVEUBnyMDI1HNPo3BRAQhxgylA+cmdjEyeq6FTKY62r0sBemw=="
+  val s2 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJpc3MiLCJzdWIiOiJzdWIiLCJhdWQiOiJhdWQiLCJhdWQiOlsiYXVkIiwiYXVkMiJdLCJleHAiOjQyLCJuYmYiOjQyLCJpYXQiOjQyLCJqdGkiOiJqdGkiLCJmb28iOiJiYXIifQ==.9SCDyruJ9p0SGkzGdMdBc6O5wLK1G7MKtGuNABVEUBnyMDI1HNPo3BRAQhxgylA+cmdjEyeq6FTKY62r0sBw=="
+  val s3 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9eyJpc3MiOiJpc3MiLCJzdWIiOiJzdWIiLCJhdWQiOiJhdWQiLCJhdWQiOlsiYXVkIiwiYXVkMiJdLCJleHAiOjQyLCJuYmYiOjQyLCJpYXQiOjQyLCJqdGkiOiJqdGkiLCJmb28iOiJiYXIifQ==.9SCDyruJ9p0SGkzGdMdBc6O5wLK1G7MKtGuNABVEUBnyMDI1HNPo3BRAQhxgylA+cmdjEyeq6FTKY62r0sBw=="
 
 
+  println(s)
 
 
+  val r1 = Jws.validate(s)
+  val r2 = Jws.validate(s2)
+  val r3 = Jws.validate(s3)
+
+
+  println(r1)  // \/-(())
+  println(r2)  // -\/(InvalidSignature)
+  println(r3)  // -\/(InvalidJwsCompact)
 
 
 }
