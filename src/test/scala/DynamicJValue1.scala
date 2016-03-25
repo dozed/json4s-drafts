4e43e61b8884539da6eb7bde1e0edb7800e43f28
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-03-25 16:56:59 +0100

    DynamicJValue

---
+++ src/test/scala/DynamicJValue1.scala
@@ -0,0 +1,41 @@
+
+object DynamicJValue1 extends App {
+
+  import org.json4s._
+  import org.json4s.jackson.{parseJson, compactJson}
+  import org.json4s.ext.scalaz.JsonScalaz._
+  import scalaz._, Scalaz._
+
+  val text =
+    """
+      |{
+      |  "s" : "string",
+      |  "i" : 123,
+      |  "dyn" : { "ss": "another string" }
+      |}
+    """.stripMargin
+
+  case class Boo(s: String, i: Int, dyn: DynamicJValue)
+
+  implicit val dynamicJValueRead = JSON.read[DynamicJValue] { json =>
+    DynamicJValue.dyn(json).successNel
+  }
+
+  implicit val dynamicJValueWrite = JSON.write[DynamicJValue] { dyn =>
+    dyn.raw
+  }
+
+  val json = parseJson(text)
+
+
+  json.read[Boo] map { boo =>
+
+    println(boo.dyn.ss.raw)
+    // JString(another string)
+
+    println(compactJson(boo.toJson))
+    // {"s":"string","i":123,"dyn":{"ss":"another string"}}
+
+  }
+
+}
